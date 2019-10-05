a8c7cd259cfea3415297d364cddf2cec2b02ff3c
Ivan <jkostylev592@gmail.com>
2019-10-05 11:46:32 +0300

    Реализована функция encrypt_caesar

--- homework01/caesar.py
+++ homework01/caesar.py
@@ -7,36 +7,17 @@ def encrypt_caesar(plaintext: str) -> str:
     >>> encrypt_caesar("Python3.6")
     'Sbwkrq3.6'
     >>> encrypt_caesar("")
-    ''def encrypt_caesar(plaintext: str) -> str:
-    """
-    >>> encrypt_caesar("PYTHON")
-    'SBWKRQ'
-    >>> encrypt_caesar("python")
-    'sbwkrq'
-    >>> encrypt_caesar("Python3.6")
-    'Sbwkrq3.6'
-    >>> encrypt_caesar("")
     ''
     """
-    # PUT YOUR CODE HERE
-    return ciphertext
-
-
-def decrypt_caesar(ciphertext: str) -> str:
-    """
-    >>> decrypt_caesar("SBWKRQ")
-    'PYTHON'
-    >>> decrypt_caesar("sbwkrq")
-    'python'
-    >>> decrypt_caesar("Sbwkrq3.6")
-    'Python3.6'
-    >>> decrypt_caesar("")
-    ''
-    """
-    # PUT YOUR CODE HERE
-    return plaintext
-    """
-    # PUT YOUR CODE HERE
+    # MAIN PROGRAM
+    shift = 3 
+    ciphertext = ""
+    for i in range (0, len(plaintext)):
+        letter = ord(plaintext[i]) # spell the word
+        if (letter >= ord("A") & letter <= ord("Z") | letter >= ord("a") & letter <= ord("z")): 
+            # check the letter and form new 'ord' of letter
+            letter = letter + shift
+        ciphertext += letter # add the new letter to encrypt word
     return ciphertext
 
 
