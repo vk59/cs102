81bea85a6495eb12b00a2eab97a43e88d515307d
Ivan <jkostylev592@gmail.com>
2019-10-05 11:18:07 +0300

    Добавлен шаблон для шифра Цезаря

---
+++ homework01/caesar.py
@@ -0,0 +1,55 @@
+def encrypt_caesar(plaintext: str) -> str:
+    """
+    >>> encrypt_caesar("PYTHON")
+    'SBWKRQ'
+    >>> encrypt_caesar("python")
+    'sbwkrq'
+    >>> encrypt_caesar("Python3.6")
+    'Sbwkrq3.6'
+    >>> encrypt_caesar("")
+    ''def encrypt_caesar(plaintext: str) -> str:
+    """
+    >>> encrypt_caesar("PYTHON")
+    'SBWKRQ'
+    >>> encrypt_caesar("python")
+    'sbwkrq'
+    >>> encrypt_caesar("Python3.6")
+    'Sbwkrq3.6'
+    >>> encrypt_caesar("")
+    ''
+    """
+    # PUT YOUR CODE HERE
+    return ciphertext
+
+
+def decrypt_caesar(ciphertext: str) -> str:
+    """
+    >>> decrypt_caesar("SBWKRQ")
+    'PYTHON'
+    >>> decrypt_caesar("sbwkrq")
+    'python'
+    >>> decrypt_caesar("Sbwkrq3.6")
+    'Python3.6'
+    >>> decrypt_caesar("")
+    ''
+    """
+    # PUT YOUR CODE HERE
+    return plaintext
+    """
+    # PUT YOUR CODE HERE
+    return ciphertext
+
+
+def decrypt_caesar(ciphertext: str) -> str:
+    """
+    >>> decrypt_caesar("SBWKRQ")
+    'PYTHON'
+    >>> decrypt_caesar("sbwkrq")
+    'python'
+    >>> decrypt_caesar("Sbwkrq3.6")
+    'Python3.6'
+    >>> decrypt_caesar("")
+    ''
+    """
+    # PUT YOUR CODE HERE
+    return plaintext
