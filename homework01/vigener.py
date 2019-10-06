6e5bab0c539d632d30390f636140362d8d3d09f7
Ivan <jkostylev592@gmail.com>
2019-10-06 20:47:19 +0300

    Релизация функции encrypt_vigenere()

--- homework01/vigener.py
+++ homework01/vigener.py
@@ -7,7 +7,22 @@ def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     >>> encrypt_vigenere("ATTACKATDAWN", "LEMON")
     'LXFOPVEFRNHR'
     """
-    # PUT YOUR CODE HERE
+    # MY CODE:
+    ciphertext=""
+    keys=[]
+    # transform keyword to list
+    for i in range (0,len(keyword)):
+        if (keyword [i] <= 'Z'):
+            keys.append (ord(keyword[i]) - ord ('A'))
+        else:
+            keys.append (ord(keyword[i]) - ord ('a'))
+    for i in range (0, len(plaintext)):
+        num_letter = ord(plaintext [i])
+        if (num_letter >= ord('A')) & (num_letter <= ord('Z')): # checking the symbol
+            num_letter = (num_letter + keys[i%len(keys)] - ord ('A')) % 26 + ord ('A') 
+        elif (num_letter >= ord('a')) & (num_letter <= ord('z')): # checking the symbol
+            num_letter = (num_letter + keys[i%len(keys)] - ord ('a')) % 26 + ord ('a') 
+        ciphertext += chr (num_letter)
     return ciphertext
 
 
