0ef369e47bc769c090f81d2ee62c0e43cb41d6bf
Ivan <jkostylev592@gmail.com>
2019-10-06 17:48:58 +0300

    Исправлены ошибки в функции encrypt_caesar и добавлена функция decrypt_caesar(). Тесты выполнены

--- homework01/caesar.py
+++ homework01/caesar.py
@@ -14,9 +14,11 @@ def encrypt_caesar(plaintext: str) -> str:
     ciphertext = ""
     for i in range (0, len(plaintext)):
         letter = ord(plaintext[i]) # spell the word
-        if (letter >= ord("A") & letter <= ord("Z") | letter >= ord("a") & letter <= ord("z")): 
+        if (letter >= ord('A')) & (letter <= ord('Z') ): 
             # check the letter and form new 'ord' of letter
-            letter = letter + shift
+            letter = (letter - ord ('A') + shift) % 26 + ord ('A')
+        elif (letter >= ord('a')) & (letter <= ord('z')):
+            letter = (letter - ord ('a') + shift) % 26 + ord ('a')
         ciphertext += chr(letter) # add the new letter to encrypt word
     return ciphertext
 
@@ -33,6 +35,20 @@ def decrypt_caesar(ciphertext: str) -> str:
     ''
     """
     # MY CODE decrypt:
-
-
+    shift = 3
+    plaintext = ""
+    letter = ''
+    for i in range (0, len(ciphertext)):
+        letter = ord(ciphertext[i])
+        if (letter >= ord('A')) & (letter <= ord('Z') ):
+            letter = letter - ord ('A') - shift
+            if letter < 0:
+                letter = 26 + letter
+            letter += ord ('A')
+        elif (letter >= ord('a')) & (letter <= ord('z') ):
+            letter = letter - ord ('a') - shift
+            if letter < 0:
+                letter = 26 + letter
+            letter += ord ('a')
+        plaintext += chr (letter)
     return plaintext
