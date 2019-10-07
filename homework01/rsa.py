be75e78171a5a190f59a202e48d21a3ec2ae3e9c
Ivan <jkostylev592@gmail.com>
2019-10-07 15:28:38 +0300

    Реализованы все функции

--- homework01/rsa.py
+++ homework01/rsa.py
@@ -34,28 +34,26 @@ def gcd(a: int, b: int) -> int:
         b = r
     return a
 
-def generate_keypair(p: int, q: int) -> Tuple[Tuple[int, int], Tuple[int, int]]:
-    if not (is_prime(p) and is_prime(q)):
-        raise ValueError('Both numbers must be prime.')
-    elif p == q:
-        raise ValueError('p and q cannot be equal')
-
-    # n = pq
-    n = p * q
-
-    # phi = (p-1)(q-1)
-    phi = (p-1) * (q-1)
-    # Choose an integer e such that e and phi(n) are coprime
-    e = random.randrange(1, phi)
-
-    # Use Euclid's Algorithm to verify that e and phi(n) are comprime
-    g = gcd(e, phi)
-    while g != 1:
-        e = random.randrange(1, phi)
-        g = gcd(e, phi)
-
-    # Use Extended Euclid's Algorithm to generate the private key
-    d = multiplicative_inverse(e, phi)
-    # Return public and private keypair
-    # Public key is (e, n) and private key is (d, n)
-    return ((e, n), (d, n))
+def multiplicative_inverse(e: int, phi: int) -> int:
+    """
+    >>> multiplicative_inverse(7, 40)
+    23
+    """
+    a = phi
+    b = e
+    c = []
+    k = 0
+    while (a % b != 0):
+        ost = a % b
+        c.append(a // b) # целая часть
+        a = b
+        b = ost
+        k += 1
+    x = 0
+    y = 1
+    for i in range (k-1, -1, -1):
+        temp = y
+        y = x - y * c[i]
+        x = temp
+    d = y % phi
+    return d
