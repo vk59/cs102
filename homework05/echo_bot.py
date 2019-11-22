c0f97fa64f6ededcaddfd857b9e5602275bec0c6
Ivan <jkostylev592@gmail.com>
2019-11-23 02:05:20 +0300

    pep8

--- homework05/echo_bot.py
+++ homework05/echo_bot.py
@@ -1,21 +1,14 @@
 import telebot
-
+import json
 
 with open('config.json') as con:
     config = json.load(con)
-
 telebot.apihelper.proxy = {'https': 'https://54.37.131.235:3128'}
 bot = telebot.TeleBot(config["access_token"])
-
-# Создание бота с указанным токеном доступа
-bot = telebot.TeleBot(access_token)
-
-
 # Бот будет отвечать только на текстовые сообщения
 @bot.message_handler(content_types=['text'])
 def echo(message: str) -> None:
     bot.send_message(message.chat.id, message.text)
-
 
 if __name__ == '__main__':
     bot.polling()
