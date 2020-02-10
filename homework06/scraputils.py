dc56a51c44114f52d60275eac5fb9de7751b8659
Ivan <jkostylev592@gmail.com>
2020-02-11 00:37:47 +0300

    completed extract_next_page()

--- homework06/scraputils.py
+++ homework06/scraputils.py
@@ -5,9 +5,7 @@ from bs4 import BeautifulSoup
 def extract_news(parser):
     """ Extract news from a given web page """
     news_list = []
-
-    # PUT YOUR CODE HERE
-
+    
     return news_list
 
 
@@ -15,7 +13,7 @@ def extract_next_page(parser):
     """ Extract next page URL """
     tbl_list = parser.table.findAll('table')
     a_list = tbl_list[1].findAll('a')
-    next_url = a_list[-1]
+    next_url = a_list[-1]['href']
     return next_url
 
 
