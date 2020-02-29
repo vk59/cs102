48b323a6b13697c037970b92c17f3c2721e54dc5
Ivan <jkostylev592@gmail.com>
2020-02-29 13:36:47 +0300

    Commited all changes

--- homework06/tests.py
+++ homework06/tests.py
@@ -18,7 +18,7 @@ for target, msg in data:
     y.append(target)
 X = [clean(x).lower() for x in X]
 X_train, y_train, X_test, y_test = X[:3900], y[:3900], X[3900:], y[3900:]
-model = NaiveBayesClassifier()
+model = NaiveBayesClassifier(alpha=0.05)
 model.fit(X_train, y_train)
 print(model.score(X_test, y_test))
 # 0.9820574162679426
