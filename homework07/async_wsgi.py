ff8253292967d3228358a1e01f949958b71e1627
vk59 <jkostylev592@gmail.com>
2020-03-25 16:09:51 +0300

    Initial commit

---
+++ homework07/async_wsgi.py
@@ -0,0 +1,22 @@
+class AsyncWSGIServer(httpd.AsyncServer):
+
+    def set_app(self, application):
+        self.application = application
+
+    def get_app(self):
+        return self.application
+
+
+class AsyncWSGIRequestHandler(httpd.AsyncHTTPRequestHandler):
+
+    def get_environ(self):
+        pass
+
+    def start_response(self, status, response_headers, exc_info=None):
+        pass
+
+    def handle_request(self):
+        pass
+
+    def finish_response(self, result):
+        pass
